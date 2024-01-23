2d6a4358d2778883f2cecf5378eeeb9488bb39f8
Katilmami <akbalmuhammed668@gmail.com>
2024-01-23 18:01:53 +0300

    Create ramcleaner.java

---
+++ src/main/java/com/example/ramcleaner/ramcleaner.java
@@ -0,0 +1,45 @@
+package com.example.ramcleaner;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ramcleaner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("DeepCleanMemoryPlugin enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("DeepCleanMemoryPlugin disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("deepcleanmemory")) {
+
+            sender.sendMessage("Waiting for garbage collection to clean up memory...");
+          
+            System.gc();
+
+            sender.sendMessage("RAM is being cleared, wait...");
+        
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+          
+            long afterMemory = Runtime.getRuntime().freeMemory();
+            long cleanedMemoryBytes = afterMemory;
+            long cleanedMemoryMB = cleanedMemoryBytes / (1024 * 1024);
+
+            sender.sendMessage("RAM deep cleaned: " + cleanedMemoryMB + " MB");
+            return true;
+        }
+        return false;
+    }
+}
